d3eee02a821b3ef103698b8c6958a9026007d842
GeOdin <madeleine.vanwinkel@gmail.com>
2019-06-03 10:51:20 +0200

    Use component for app and add submit function.

--- src/App.js
+++ src/App.js
@@ -1,148 +1,201 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <main className="App-main">
-        <form>
-          <div>
-            1 Wat voor model wilt u checken?
-            <br />
-            <input type="radio" name="model-type-riolering" value="riolering" />
-            Riolering
-            <br />
-            <input type="radio" name="model-type-oppervlakte-water" value="oppervlakte-water" disabled/>
-            Oppervlakte water
-            <br />
-            <button>VOLGENDE</button>
-            <br />
-            <br />
-          </div>
-          <div>
-            2 Uit wat voor data bestaat uw riolering-model?
-            <br />
-            <table>
-              <tr>
-                <td>Aanleverformaat</td>
-                <td>Aanvullende data</td>
-                <td>Verhard oppervlak</td>
-              </tr>
-              <tr>
-                <td>
-                  <input type="checkbox" name="model-data-gwsw-hydx" value="gwsw-hydx" />
-                  GWSW Hydx (aanbevolen)
-                </td>
-                <td>
-                  <input type="checkbox" name="model-data-verhard-oppervlak" value="verhard-oppervlak" />
-                  Verhard oppervlak
-                </td>
-                <td>
-                  <input type="checkbox" name="model-data-bgt-inloopmodel" value="bgt-inloopmodel" />
-                  BGT inloopmodel
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <input type="checkbox" name="model-data-suf-hydx" value="suf-hydx" />
-                  Suf Hydx (aanbevolen)
-                </td>
-                <td>
-                  <input type="checkbox" name="model-data-drinkwater-gebruik" value="drinkwater-gebruik" />
-                  Drinkwater gebruik
-                </td>
-                <td>
-                  <input type="checkbox" name="model-data-anders-2" value="anders-2" />
-                  Anders
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <input type="checkbox" name="model-data-gbi" value="gbi" />
-                  GBI
-                </td>
-                <td>
-                  <input type="checkbox" name="model-data-bemalings-gebieden" value="bemalings-gebieden" />
-                  Bemalings gebieden
-                </td>
-                <td>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <input type="checkbox" name="model-data-anders" value="anders" />
-                  Anders
-                </td>
-                <td>
-                  <input type="checkbox" name="model-data-andere-data" value="andere-data" />
-                  Andere data
-                </td>
-                <td>
-                </td>
-              </tr>
-            </table>
-            <button>VOLGENDE</button>
-            <br />
-            <br />
-          </div>
-          <div>
-            3 Upload bestanden voor GWSW Hydx
-            <br />
-            <input type="file" name="gwsw-hydx-file" value="" />
-            <br />
-            <br />
-            Upload bestanden voor Suf-Hyd
-            <br />
-            <input type="file" name="suf-hyd-file" value="" />
-            <br />
-            <br />
-            Upload bestanden voor een ander aanleverformaat
-            <br />
-            <input type="file" name="ander-file" value="" />
-            <br />
-            <br />
-            Upload bestanden voor Verhard oppervlak - BGT inloopmodel
-            <br />
-            <input type="file" name="verhard-opplervlak-bgt-inloopmodel" value="" />
-            <br />
-            <br />
-            Upload bestanden voor Drinkwater gebruik
-            <br />
-            <input type="file" name="drinkwater-gebruik-file" value="" />
-            <br />
-            <br />
-            Upload bestanden voor Andere data
-            <br />
-            <input type="file" name="andere-data-file" value="" />
-            <br />
-            <br />
-            <button>VOLGENDE</button>
-            <br />
-            <br />
-          </div>
-          <div>
-            4 Naam en email-adres
-            <br />
-            <input type="text" name="name" value="" placeholder="Name" />
-            <br />
-            <input type="text" name="email" value="" placeholder="Email" />
-            <br />
-            <button>VOLGENDE</button>
-            <br />
-            <br />
-          </div>
-          <div>
-            U bent nu klaar. Controleer nog een keer uw aangeleverde data of verzend direct.
-            <button>VERZENDEN</button>
-          </div>
-        </form>
-      </main>
-      <footer className="App-footer">
-      </footer>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      name: "",
+      email: "",
+      // modelType: {
+      //   riolering: false
+      // }
+      modelTypeRiolering: false
+    };
+
+    this.handleChangeModelTypeRiolering = this.handleChangeModelTypeRiolering.bind(this);
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeModelTypeRiolering(event) {
+    this.setState({modelTypeRiolering: !event.target.modelTypeRiolering});
+  }
+
+  handleChangeName(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleChangeEmail(event) {
+    this.setState({email: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('modelTypeRiolering: ' + this.state.modelTypeRiolering);
+    console.log('Name: ' + this.state.name);
+    console.log('Email: ' + this.state.email);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <main className="App-main">
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              1 Wat voor model wilt u checken?
+              <br />
+              <input type="radio" name="model-type-riolering" value="riolering" checked={this.state.modelTypeRiolering} onChange={this.handleChangeModelTypeRiolering} />
+              Riolering
+              <br />
+              <input type="radio" name="model-type-oppervlakte-water" value="oppervlakte-water" disabled/>
+              Oppervlakte water
+              <br />
+              <button>VOLGENDE</button>
+              <br />
+              <br />
+            </div>
+            <div>
+              2 Uit wat voor data bestaat uw riolering-model?
+              <br />
+              <table>
+                <thead>
+                  <tr>
+                    <td>Aanleverformaat</td>
+                    <td>Aanvullende data</td>
+                    <td>Verhard oppervlak</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <input type="checkbox" name="model-data-gwsw-hydx" value="gwsw-hydx" />
+                      GWSW Hydx (aanbevolen)
+                    </td>
+                    <td>
+                      <input type="checkbox" name="model-data-verhard-oppervlak" value="verhard-oppervlak" />
+                      Verhard oppervlak
+                    </td>
+                    <td>
+                      <input type="checkbox" name="model-data-bgt-inloopmodel" value="bgt-inloopmodel" />
+                      BGT inloopmodel
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input type="checkbox" name="model-data-suf-hydx" value="suf-hydx" />
+                      Suf Hydx (aanbevolen)
+                    </td>
+                    <td>
+                      <input type="checkbox" name="model-data-drinkwater-gebruik" value="drinkwater-gebruik" />
+                      Drinkwater gebruik
+                    </td>
+                    <td>
+                      <input type="checkbox" name="model-data-anders-2" value="anders-2" />
+                      Anders
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input type="checkbox" name="model-data-gbi" value="gbi" />
+                      GBI
+                    </td>
+                    <td>
+                      <input type="checkbox" name="model-data-bemalings-gebieden" value="bemalings-gebieden" />
+                      Bemalings gebieden
+                    </td>
+                    <td>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input type="checkbox" name="model-data-anders" value="anders" />
+                      Anders
+                    </td>
+                    <td>
+                      <input type="checkbox" name="model-data-andere-data" value="andere-data" />
+                      Andere data
+                    </td>
+                    <td>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <button>VOLGENDE</button>
+              <br />
+              <br />
+            </div>
+            <div>
+              3 Upload bestanden voor GWSW Hydx
+              <br />
+              <input type="file" name="gwsw-hydx-file" />
+              <br />
+              <br />
+              Upload bestanden voor Suf-Hyd
+              <br />
+              <input type="file" name="suf-hyd-file" />
+              <br />
+              <br />
+              Upload bestanden voor een ander aanleverformaat
+              <br />
+              <input type="file" name="ander-file" />
+              <br />
+              <br />
+              Upload bestanden voor Verhard oppervlak - BGT inloopmodel
+              <br />
+              <input type="file" name="verhard-opplervlak-bgt-inloopmodel" />
+              <br />
+              <br />
+              Upload bestanden voor Drinkwater gebruik
+              <br />
+              <input type="file" name="drinkwater-gebruik-file" />
+              <br />
+              <br />
+              Upload bestanden voor Andere data
+              <br />
+              <input type="file" name="andere-data-file" />
+              <br />
+              <br />
+              <button>VOLGENDE</button>
+              <br />
+              <br />
+            </div>
+            <div>
+              4 Naam en email-adres
+              <br />
+              <input type="text" name="name" value={this.state.name} onChange={this.handleChangeName} placeholder="Name" />
+              <br />
+              <input type="text" name="email" value={this.state.email} onChange={this.handleChangeEmail} placeholder="Email" />
+              <br />
+              <button>VOLGENDE</button>
+              <br />
+              <br />
+            </div>
+            <div>
+              5 Folder naam
+              <br />
+              <input type="text" name="folder-name" value="" placeholder="Folder" />
+              <br />
+              <button>VOLGENDE</button>
+              <br />
+              <br />
+            </div>
+            <div>
+              U bent nu klaar. Controleer nog een keer uw aangeleverde data of verzend direct.
+              <input type="submit" value="VERZENDEN" />
+            </div>
+          </form>
+        </main>
+        <footer className="App-footer">
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
