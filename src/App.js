c8f71502d9dc497c61220cab948119a79fd0c4c5
Madeleine van Winkel <madeleine.vanwinkel@nelen-schuurmans.nl>
2019-06-04 09:51:00 +0200

    Madeleine form without typescript (feature branch) (#4)
    * Create form for uploading data.
    
    * Use component for app and add submit function.
    
    * Add post (without credentials and without the mandatory at least 1 file, so it gives 403 forbidden.
    
    * Add start for authentication with bootstrap.
    
    * Add the most-wanted .gitignore.
    
    * sso api calls
    
    * add proxy
    
    * redirect to sso
    
    * add basic authentication to dev
    
    * upload file draft
    
    * comment out basic auth
    
    * form without typescript form (#6)
    
    * Add change handlers for step 2.
    
    * Make it possible to add multiple files for 1 input type = file.
    
    * Fix bug: checkboxes are now properly toggable, also add (multiple) files for first input type file of Step 3.
    
    * Add files from step 3 to the state.
    
    * Only make it possible to upload 1 file per category for the MVP.
    
    * Style header and footer and remove next buttons that are not used.
    
    * Add styling for step counters and step titles.
    
    * Add package-lock.json.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,402 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { getPassword } from './GetPassword.js'
+import { getUserName } from './GetUsername.js' 
+
+
+class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      // Step 1
+      modelTypeRiolering: false,
+      modelTypeOppervlakteWater: false,
+      // Step 2
+      deliveryFormatGWSW: false,
+      deliveryFormatSuf: false,
+      deliveryFormatGBI: false,
+      deliveryFormatOther: false,
+      additionalDataHardenedSurface: false,
+      additionalDataDrinkingWaterUsage: false,
+      additionalDataDrainageAreas: false,
+      additionalDataOtherData: false,
+      hardenedSurfaceBGT: false,
+      hardenedSurfaceOther: false,
+      // Step 3
+      fileDeliveryFormatGWSW: [],
+      fileDeliveryFormatSuf: [],
+      fileDeliveryFormatGBI: [],
+      fileDeliveryFormatOther: [],
+      fileAdditionalDataHardenedSurface: [],
+      fileAdditionalDataDrinkingWaterUsage: [],
+      fileAdditionalDataDrainageAreas: [],
+      fileAdditionalDataOtherData: [],
+      fileHardenedSurfaceBGT: [],
+      fileHardenedSurfaceOther: [],
+      // Step 4
+      name: "",
+      email: "",
+      // Step 5
+      folderName: "",
+    };
+
+    // Step 1
+    this.handleChangeModelTypeRiolering = this.handleChangeModelTypeRiolering.bind(this);
+    this.handleChangeModelTypeOppervlakteWater = this.handleChangeModelTypeOppervlakteWater.bind(this);
+    // Step 2
+    this.handleChangeDeliveryFormatGWSW = this.handleChangeDeliveryFormatGWSW.bind(this);
+    this.handleChangeDeliveryFormatSuf = this.handleChangeDeliveryFormatSuf.bind(this);
+    this.handleChangeDeliveryFormatGBI = this.handleChangeDeliveryFormatGBI.bind(this);
+    this.handleChangedeliveryFormatOther = this.handleChangedeliveryFormatOther.bind(this);
+    this.handleChangeAdditionalDataHardenedSurface = this.handleChangeAdditionalDataHardenedSurface.bind(this);
+    this.handleChangeAdditionalDataDrinkingWaterUsage = this.handleChangeAdditionalDataDrinkingWaterUsage.bind(this);
+    this.handleChangeAdditionalDataDrainageAreas = this.handleChangeAdditionalDataDrainageAreas.bind(this);
+    this.handleChangeAdditionalDataOtherData = this.handleChangeAdditionalDataOtherData.bind(this);
+    this.handleChangeHardenedSurfaceBGT = this.handleChangeHardenedSurfaceBGT.bind(this);
+    this.handleChangeHardenedSurfaceOther = this.handleChangeHardenedSurfaceOther.bind(this);
+    // Step 3
+    this.handleChangeFileDeliveryFormatGWSW = this.handleChangeFileDeliveryFormatGWSW.bind(this);
+    this.handleChangeFileDeliveryFormatSuf = this.handleChangeFileDeliveryFormatSuf.bind(this);
+    this.handleChangeFileDeliveryFormatGBI = this.handleChangeFileDeliveryFormatGBI.bind(this);
+    this.handleChangeFileDeliveryFormatOther = this.handleChangeFileDeliveryFormatOther.bind(this);
+    this.handleChangeFileAdditionalDataHardenedSurface = this.handleChangeFileAdditionalDataHardenedSurface.bind(this);
+    this.handleChangeFileAdditionalDataDrinkingWaterUsage = this.handleChangeFileAdditionalDataDrinkingWaterUsage.bind(this);
+    this.handleChangeFileAdditionalDataDrainageAreas = this.handleChangeFileAdditionalDataDrainageAreas.bind(this);
+    this.handleChangeFileAdditionalDataOtherData = this.handleChangeFileAdditionalDataOtherData.bind(this);
+    this.handleChangeFileHardenedSurfaceBGT = this.handleChangeFileHardenedSurfaceBGT.bind(this);
+    this.handleChangeFileHardenedSurfaceOther = this.handleChangeFileHardenedSurfaceOther.bind(this);
+    // Step 4
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    // Step 5
+    this.handleChangeFolderName = this.handleChangeFolderName.bind(this);
+    // Submit
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  
+  componentDidMount() {
+    
+    const url = "/api/upload/"
+    const opts = {
+      credentials: "same-origin",
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch(url, opts)
+      .then(responseParsed => {
+        return responseParsed.json();
+      })
+      .then(parsedBody => {
+        if (parsedBody.detail === "Authentication credentials were not provided.") {
+          console.log("Authentication credentials were not provided.");
+          const nextUrl = window.location.href;
+          // next line needs be active on prod, but commented out on dev
+          window.location.href = `${"/accounts/login/"}?next=${nextUrl}`;
+        } else {
+          console.log("You appear to be logged in");
+        }
+
+        
+
+
+      });
+  }
+
+  // Step 1
+  handleChangeModelTypeRiolering(event) {
+    this.setState({modelTypeRiolering: !this.state.modelTypeRiolering});
+  }
+  handleChangeModelTypeOppervlakteWater(event) {
+    this.setState({modelTypeOppervlakteWater: !this.state.modelTypeOppervlakteWater});
+  }
+
+  // Step 2
+  handleChangeDeliveryFormatGWSW(event) {
+    this.setState({deliveryFormatGWSW: !this.state.deliveryFormatGWSW});
+  }
+  handleChangeDeliveryFormatSuf(event) {
+    this.setState({deliveryFormatSuf: !this.state.deliveryFormatSuf});
+  }
+  handleChangeDeliveryFormatGBI(event) {
+    this.setState({deliveryFormatGBI: !this.state.deliveryFormatGBI});
+  }
+  handleChangedeliveryFormatOther(event) {
+    this.setState({deliveryFormatOther: !this.state.deliveryFormatOther});
+  }
+  handleChangeAdditionalDataHardenedSurface(event) {
+    this.setState({additionalDataHardenedSurface: !this.state.additionalDataHardenedSurface});
+  }
+  handleChangeAdditionalDataDrinkingWaterUsage(event) {
+    this.setState({additionalDataDrinkingWaterUsage: !this.state.additionalDataDrinkingWaterUsage});
+  }
+  handleChangeAdditionalDataDrainageAreas(event) {
+    this.setState({additionalDataDrainageAreas: !this.state.additionalDataDrainageAreas});
+  }
+  handleChangeAdditionalDataOtherData(event) {
+    this.setState({additionalDataOtherData: !this.state.additionalDataOtherData});
+  }
+  handleChangeHardenedSurfaceBGT(event) {
+    this.setState({hardenedSurfaceBGT: !this.state.hardenedSurfaceBGT});
+  }
+  handleChangeHardenedSurfaceOther(event) {
+    this.setState({hardenedSurfaceOther: !this.state.hardenedSurfaceOther});
+  }
+
+  // Step 3
+  handleChangeFileDeliveryFormatGWSW(event) {
+    this.setState({fileDeliveryFormatGWSW: event.target.files});
+  }
+  handleChangeFileDeliveryFormatSuf(event) {
+    this.setState({fileDeliveryFormatSuf: event.target.files});
+  }
+  handleChangeFileDeliveryFormatGBI(event) {
+    this.setState({fileDeliveryFormatGBI: event.target.files});
+  }
+  handleChangeFileDeliveryFormatOther(event) {
+    this.setState({fileDeliveryFormatOther: event.target.files});
+  }
+  handleChangeFileAdditionalDataHardenedSurface(event) {
+    this.setState({fileAdditionalDataHardenedSurface: event.target.files});
+  }
+  handleChangeFileAdditionalDataDrinkingWaterUsage(event) {
+    this.setState({fileAdditionalDataDrinkingWaterUsage: event.target.files});
+  }
+  handleChangeFileAdditionalDataDrainageAreas(event) {
+    this.setState({fileAdditionalDataDrainageAreas: event.target.files});
+  }
+  handleChangeFileAdditionalDataOtherData(event) {
+    this.setState({fileAdditionalDataOtherData: event.target.files});
+  }
+  handleChangeFileHardenedSurfaceBGT(event) {
+    this.setState({fileHardenedSurfaceBGT: event.target.files});
+  }
+  handleChangeFileHardenedSurfaceOther(event) {
+    this.setState({fileHardenedSurfaceOther: event.target.files});
+  }
+
+  // Step 4
+  handleChangeName(event) {
+    this.setState({name: event.target.value});
+  }
+  handleChangeEmail(event) {
+    this.setState({email: event.target.value});
+  }
+
+  // Step 5
+  handleChangeFolderName(event) {
+    this.setState({folderName: event.target.value}); // gives error that this is undefined..
+  }
+
+  // Submit
+  handleSubmit(event) {
+    console.log(this.state);
+
+    var fileInput = document.getElementsByName('suf-hyd-file')[0];
+    console.log('fileInput', fileInput, fileInput.files, fileInput.files[0]);
+    var form = new FormData();
+    form.append("file", fileInput.files[0]);
+    const url = "/api/upload/"
+    const opts = {
+      credentials: "same-origin",
+      method: "POST",
+      // headers: { 
+      //   'Authorization': 'Basic ' + btoa(getUserName() + ":" + getPassword())
+      // },
+      body: form
+    };
+    fetch(url, opts)
+    .then(responseParsed => {
+      return responseParsed.json();
+    })
+    .then(parsedBody => {
+      console.log(parsedBody)
+    })
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-header-top">
+            Datachecker
+          </div>
+          <div className="App-header-bottom"></div>
+        </header>
+        <main className="App-main">
+          <form enctype="multipart/form-data" onSubmit={this.handleSubmit}>
+            <div>
+              <br />
+              <div className="step-counter">
+                <span className="step-counter-span">1</span>
+                <span className="step-counter-title">Wat voor model wilt u checken?</span>
+              </div>
+              <input type="radio" name="model-type-riolering" value="riolering" checked={this.state.modelTypeRiolering} onChange={this.handleChangeModelTypeRiolering} />
+              Riolering
+              <br />
+              <input type="radio" name="model-type-oppervlakte-water" value="oppervlakte-water" checked={this.state.modelTypeOppervlakteWater} onChange={this.handleChangeModelTypeOppervlakteWater} disabled/>
+              Oppervlakte water
+            </div>
+            <br />
+            <div>
+              <div className="step-counter">
+                <span className="step-counter-span">2</span>
+                <span className="step-counter-title">Uit wat voor data bestaat uw riolering-model?</span>
+              </div>
+              <table>
+                <thead>
+                  <tr>
+                    <td>Aanleverformaat</td>
+                    <td>Aanvullende data</td>
+                    <td>Verhard oppervlak</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <input type="checkbox" name="model-data-gwsw-hydx" value="gwsw-hydx" checked={this.state.deliveryFormatGWSW} onChange={this.handleChangeDeliveryFormatGWSW} />
+                      GWSW Hydx (aanbevolen)
+                    </td>
+                    <td>
+                      <input type="checkbox" name="model-data-verhard-oppervlak" value="verhard-oppervlak" checked={this.state.additionalDataHardenedSurface} onChange={this.handleChangeAdditionalDataHardenedSurface} />
+                      Verhard oppervlak
+                    </td>
+                    <td>
+                      <input type="checkbox" name="model-data-bgt-inloopmodel" value="bgt-inloopmodel" checked={this.state.hardenedSurfaceBGT} onChange={this.handleChangeHardenedSurfaceBGT} />
+                      BGT inloopmodel
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input type="checkbox" name="model-data-suf-hydx" value="suf-hydx" checked={this.state.deliveryFormatSuf} onChange={this.handleChangeDeliveryFormatSuf} />
+                      Suf Hydx (aanbevolen)
+                    </td>
+                    <td>
+                      <input type="checkbox" name="model-data-drinkwater-gebruik" value="drinkwater-gebruik" checked={this.state.additionalDataDrinkingWaterUsage} onChange={this.handleChangeAdditionalDataDrinkingWaterUsage} />
+                      Drinkwater gebruik
+                    </td>
+                    <td>
+                      <input type="checkbox" name="model-data-anders-2" value="anders-2" checked={this.state.hardenedSurfaceOther} onChange={this.handleChangeHardenedSurfaceOther} />
+                      Anders
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input type="checkbox" name="model-data-gbi" value="gbi" checked={this.state.deliveryFormatGBI} onChange={this.handleChangeDeliveryFormatGBI} />
+                      GBI
+                    </td>
+                    <td>
+                      <input type="checkbox" name="model-data-bemalings-gebieden" value="bemalings-gebieden" checked={this.state.additionalDataDrainageAreas} onChange={this.handleChangeAdditionalDataDrainageAreas} />
+                      Bemalings gebieden
+                    </td>
+                    <td>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input type="checkbox" name="model-data-anders" value="anders" checked={this.state.deliveryFormatOther} onChange={this.handleChangedeliveryFormatOther} />
+                      Anders
+                    </td>
+                    <td>
+                      <input type="checkbox" name="model-data-andere-data" value="andere-data" checked={this.state.additionalDataOtherData} onChange={this.handleChangeAdditionalDataOtherData} />
+                      Andere data
+                    </td>
+                    <td>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <br />
+            <div>
+              <div className="step-counter">
+                <span className="step-counter-span">3</span>
+                <span className="step-counter-title">Upload bestanden voor GWSW Hydx</span>
+              </div>
+              <input type="file" name="gwsw-hydx-file" onChange={this.handleChangeFileDeliveryFormatGWSW} />
+              <br />
+              <br />
+              Upload bestanden voor Suf-Hyd
+              <br />
+              <input type="file" name="suf-hyd-file" onChange={this.handleChangeFileDeliveryFormatSuf} />
+              <br />
+              <br />
+              Upload bestanden voor GBI
+              <br />
+              <input type="file" name="gbi-file" onChange={this.handleChangeFileDeliveryFormatGBI} />
+              <br />
+              <br />
+              Upload bestanden voor een ander aanleverformaat
+              <br />
+              <input type="file" name="ander-file" onChange={this.handleChangeFileDeliveryFormatOther} />
+              <br />
+              <br />
+              Upload bestanden voor Verhard oppervlak
+              <br />
+              <input type="file" name="verhard-oppervlak-file" onChange={this.handleChangeFileAdditionalDataHardenedSurface} />
+              <br />
+              <br />
+              Upload bestanden voor Verhard oppervlak - BGT inloopmodel
+              <br />
+              <input type="file" name="verhard-oppervlak-bgt-file" onChange={this.handleChangeFileHardenedSurfaceBGT} />
+              <br />
+              <br />
+              Upload bestanden voor Verhard oppervlak - Anders
+              <br />
+              <input type="file" name="verhard-oppervlak-anders-file" onChange={this.handleChangeFileHardenedSurfaceOther} />
+              <br />
+              <br />
+              Upload bestanden voor Drinkwater gebruik
+              <br />
+              <input type="file" name="drinkwater-gebruik-file" onChange={this.handleChangeFileAdditionalDataDrinkingWaterUsage} />
+              <br />
+              <br />
+              Upload bestanden voor Bemalings Gebieden
+              <br />
+              <input type="file" name="bemalings-gebieden-file" onChange={this.handleChangeFileAdditionalDataDrainageAreas} />
+              <br />
+              <br />
+              Upload bestanden voor Andere data
+              <br />
+              <input type="file" name="andere-data-file" onChange={this.handleChangeFileAdditionalDataOtherData} />
+            </div>
+            <br />
+            <div>
+              <div className="step-counter">
+                <span className="step-counter-span">4</span>
+                <span className="step-counter-title">Naam en email-adres</span>
+              </div>
+              <input type="text" name="name" value={this.state.name} onChange={this.handleChangeName} placeholder="Name" />
+              <br />
+              <input type="text" name="email" value={this.state.email} onChange={this.handleChangeEmail} placeholder="Email" />
+            </div>
+            <br />
+            <div>
+              <div className="step-counter">
+                <span className="step-counter-span">5</span>
+                <span className="step-counter-title">Folder naam</span>
+              </div>
+              <input type="text" name="folderName" value={this.state.folderName} onChange={this.handleChangeFolderName} placeholder="Folder" />
+            </div>
+            <br />
+            <br />
+            <div>
+              U bent nu klaar. Controleer nog een keer uw aangeleverde data of verzend direct.
+              <br />
+              <br />
+              <input type="submit" value="VERZENDEN" />
+            </div>
+            <br />
+            <br />
+          </form>
+        </main>
+        <footer className="App-footer">
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
